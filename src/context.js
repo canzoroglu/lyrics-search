e75bdc2e17a27235a9ace310b32fd5e2f219ce67
canzoroglu <canzoroglu@gmail.com>
2018-12-20 14:35:58 +0300

    Fetch the data_status: 401

--- src/context.js
+++ src/context.js
@@ -4,11 +4,18 @@ const Context = React.createContext();
 
 export class Provider extends Component {
   state = {
-    track_list: [
-      {track: {track_name: "abc"}},
-      {track: {track_name: "432qe"}},
-    ],
+    track_list: [],
     heading: "Top 10 Results"
+  }
+  componentDidMount(){
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&
+      page=1&page_size=10&country=us&f_has_lyrics=1&apikey=af51884ebf5dbcb62e97871ae7b6b800`)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(JSON.stringify(myJson));
+      });
   }
   render(){
     return (
